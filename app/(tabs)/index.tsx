3da7116e09d0a4ef622cdb9fb2d5875f7e07dda4
Mohammad Khalid <blykhalid1999@gmail.com>
2025-09-01 17:51:32 +0530

    create todo list header component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,22 @@
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import Header from "@/components/header";
 import { useTheme } from "@/hooks/use-theme";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
+  const homeStyles = createHomeStyles(colors);
   return (
-    <View style={style.container}>
-      <Text style={style.content}>Hello </Text>
-      <TouchableOpacity onPress={toggleDarkMode} style={style.content}>
-        Toggle
-      </TouchableOpacity>
-    </View>
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={homeStyles.container}
+    >
+      <StatusBar barStyle={colors.statusBarStyle} />
+      <SafeAreaView style={homeStyles.safeArea}>
+        <Header />
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
-
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 10,
-  },
-  content: {
-    color: "red",
-    fontSize: 22,
-    marginBottom: 20,
-    fontWeight: "900",
-  },
-});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,18 @@
 import { ThemeProvider } from "@/hooks/use-theme";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Stack } from "expo-router";
+
+const address = "https://small-mouse-123.convex.cloud";
+const convex = new ConvexReactClient(address);
 
 export default function RootLayout() {
   return (
-    <ThemeProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-      </Stack>
-    </ThemeProvider>
+    <ConvexProvider client={convex}>
+      <ThemeProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </ThemeProvider>
+    </ConvexProvider>
   );
 }
